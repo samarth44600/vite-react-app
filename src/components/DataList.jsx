b5e58e2da0ff35b9b61ce7cefeb74e7a3aadcd15
Samarth <subedi42samarth@gmail.com>
2026-07-17 08:15:40 +0545

    implemented search feature

--- src/components/DataList.jsx
+++ src/components/DataList.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { data } from '../data/data'
 import DataCard from './DataCard'
 
 const DataList = () => {
-    const filteredData = []
+    const [searchQuery, setSearchQuery] = useState("")
+    const [filteredData, setFilteredData] = useState(data)
+
+    const handleSearch = () => {
+        // const filter = searchQuery ? data.filter((d) => d.title == searchQuery) : data
+        const filter = searchQuery ? data.filter((d) => d.title.trim().toLowerCase().includes(
+            searchQuery.trim().toLowerCase()
+        )) : data
+        setFilteredData(filter)
+        console.log("filteredData", filteredData)
+    }
     return (
         <div>
+            <input type='text' value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)} />
+            <button onClick={handleSearch} >Search</button>
             {
                 filteredData.map((d, i) => <DataCard
                     key={i}
